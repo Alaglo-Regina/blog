5e5efe32c13a6679735a0486a3cea820c6e7fac9
Alaglo Regina <reginaalaglo@gmail.com>
2024-05-16 11:17:53 +0000

    Mise en place de l'interface de connexion

---
+++ pages/connexion.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../assets/css/style.css">
+    <title>Connexion</title>
+</head>
+<body>
+    <?php 
+        require_once '../include/menu.php';
+    ?>
+
+    <div class="onSign">
+
+        <form action="">
+            <label for="username">Nom d'utilisateur</label>
+            <input type="text" name="username" id="username" placeholder="Saisir le nom d'utilisateur ou le pseudnyme ici" minlength="5" maxlength="20" required>
+
+            <label for="password">Mot de passe</label>
+            <input type="password" name="password" id="password" placeholder="Saisir votre mot de passe ici" minlength="8" maxlength="20" required>
+        </form>
+        
+    </div>
+
+</body>
+</html>
